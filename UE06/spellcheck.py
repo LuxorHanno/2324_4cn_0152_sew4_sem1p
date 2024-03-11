ed43e3b77ec9be99e6a0f38d0c05bb3d8ace794a
hanno <0152@htl.rennweg.at>
2024-03-11 11:07:39 +0100

    UE: spellcheck.py; Com.:5 Erstellung von Meth. edit2_good

--- UE06/spellcheck.py
+++ UE06/spellcheck.py
@@ -66,7 +66,6 @@ class spellcheck:
         inserts = [a + c + b for a, b in splits for c in alphabet]
         return set(deletes + transposes + replaces + inserts)
 
-
     def edit1_good(self, wort: str, alle_woerter: list[str] = None) -> set[str]:
         """
         Diese Methode gibt eine Menge von Wörtern zurück, die einen Editierabstand von 1 haben und im Wörterbuch stehen.
