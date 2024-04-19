2b1108ddd3a2ee08c7604a9df6a9e06336e9233e
Hanno <hanno.postl@icloud.com>
2024-04-19 20:10:35 +0200

    UE: Fraction.py; Com.:4 Erstellung der __r*__ Funktionen; Fertigstellung der Übung

--- UE07/Fraction.py
+++ UE07/Fraction.py
@@ -1,6 +1,7 @@
 __author__ = "Hanno Postl"
-__version__ = "1.0"
+__version__ = "1.6"
 __status__ = "work in progress"
+
 
 
 class Fraction:
@@ -15,6 +16,52 @@ class Fraction:
     1/4
     >>> f1+f2
     Fraction(3, 4)
+    >>> f1-f2
+    Fraction(1, 4)
+    >>> f1*f2
+    Fraction(1, 8)
+    >>> f1/f2
+    Fraction(2, 1)
+    >>> f1//f2
+    2
+    >>> f1%f2
+    Fraction(0, 1)
+    >>> f1 == f2
+    False
+    >>> f1 != f2
+    True
+    >>> f1 < f2
+    False
+    >>> f1 <= f2
+    False
+    >>> f1 > f2
+    True
+    >>> f1 >= f2
+    True
+    >>> 1 + Fraction(1,2)
+    Fraction(3, 2)
+    >>> Fraction(1,2) + 1
+    Fraction(3, 2)
+    >>> 1 - Fraction(1,2)
+    Fraction(1, 2)
+    >>> Fraction(1,2) - 1
+    Fraction(-1, 2)
+    >>> 1 * Fraction(1,2)
+    Fraction(1, 2)
+    >>> Fraction(1,2) * 1
+    Fraction(1, 2)
+    >>> 1 / Fraction(1,2)
+    Fraction(2, 1)
+    >>> Fraction(1,2) / 1
+    Fraction(1, 2)
+    >>> 1 // Fraction(1,2)
+    2
+    >>> Fraction(1,2) // 1
+    0
+    >>> 1 % Fraction(1,2)
+    Fraction(0, 1)
+    >>> Fraction(1,2) % 1
+    Fraction(1, 2)
     """
 
     def __init__(self, zaehler=0, nenner=1):
@@ -55,26 +102,68 @@ class Fraction:
         return Fraction(self.numerator * other.denominator + other.numerator * self.denominator,
                         self.denominator * other.denominator)
 
+    def __radd__(self, other):
+        """Addition"""
+        if isinstance(other, int):
+            return Fraction(other * self.denominator + self.numerator, self.denominator)
+        else:
+            raise NotImplementedError
+
     def __sub__(self, other):
         """Subtraktion"""
         return Fraction(self.numerator * other.denominator - other.numerator * self.denominator,
                         self.denominator * other.denominator)
 
+    def __rsub__(self, other):
+        """Subtraktion"""
+        if isinstance(other, int):
+            return Fraction(other * self.denominator - self.numerator, self.denominator)
+        else:
+            raise NotImplementedError
+
     def __mul__(self, other):
         """Multiplikation"""
         return Fraction(self.numerator * other.numerator, self.denominator * other.denominator)
+
+    def __rmul__(self, other):
+        """Multiplikation"""
+        if isinstance(other, int):
+            return Fraction(other * self.numerator, self.denominator)
+        else:
+            raise NotImplementedError
 
     def __truediv__(self, other):
         """Division"""
         return Fraction(self.numerator * other.denominator, self.denominator * other.numerator)
 
+    def __rtruediv__(self, other):
+        """Division"""
+        if isinstance(other, int):
+            return Fraction(other * self.denominator, self.numerator)
+        else:
+            raise NotImplementedError
+
     def __floordiv__(self, other):
         """Division"""
         return Fraction(self.numerator * other.denominator, self.denominator * other.numerator).__float__().__floor__()
 
+    def __rfloordiv__(self, other):
+        """Division"""
+        if isinstance(other, int):
+            return Fraction(other * self.denominator, self.numerator).__float__().__floor__()
+        else:
+            raise NotImplementedError
+
     def __mod__(self, other):
         """Modulo"""
         return Fraction((self.numerator * other.denominator)%(self.denominator * other.numerator), self.denominator * other.denominator)
+
+    def __rmod__(self, other):
+        """Modulo"""
+        if isinstance(other, int):
+            return Fraction((other * self.denominator)%self.numerator, self.denominator)
+        else:
+            raise NotImplementedError
 
     def __eq__(self, other):
         """Gleichheit"""
@@ -117,10 +206,3 @@ class Fraction:
         self._denominator = value
 
 
-if __name__ == "__main__":
-    f1 = Fraction(28, 3)
-    f2 = Fraction(4,1 )
-
-    print(f1 % f2)
-    print(9 % 4)
-    print(0.75 * 4)
