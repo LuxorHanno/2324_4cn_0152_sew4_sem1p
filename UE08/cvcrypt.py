afc8d7464956c3eacd75712de53af3dbbb6ba298
Hanno <hanno.postl@icloud.com>
2024-04-20 19:33:18 +0200

    UE: cvcrypt.py; Com.:3 Fertigstellung von cvcrypt.py

--- UE08/cvcrypt.py
+++ UE08/cvcrypt.py
@@ -12,24 +12,40 @@ from Vigenere import Vigenere
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Encrypt or decrypt a message using Caesar cipher.")
+    parser.add_argument("-p", "--procedure", choices=["caesar", "vigenere"], default="caesar", help="Encryption/Decryption procedure: caesar or vigenere")
     parser.add_argument("-m", "--mode", choices=["encrypt", "decrypt"], help="Mode of operation: encrypt or decrypt")
     parser.add_argument("-t", "--text", help="Text to be processed")
     parser.add_argument("-k", "--key", help="Key to be used for encryption or decryption")
 
     args = parser.parse_args()
 
-    caesar = Caesar()
+    if args.procedure == "vigenere":
+        vigenere = Vigenere()
 
-    if args.mode == "encrypt":
-        if args.key:
-            result = caesar.encrypt(args.text, args.key)
-        else:
-            result = caesar.encrypt(args.text)
-    elif args.mode == "decrypt":
-        if args.key:
-            result = caesar.decrypt(args.text, args.key)
-        else:
-            result = caesar.decrypt(args.text)
+        if args.mode == "encrypt":
+            if args.key:
+                result = vigenere.encrypt(args.text, args.key)
+            else:
+                result = vigenere.encrypt(args.text)
+        elif args.mode == "decrypt":
+            if args.key:
+                result = vigenere.decrypt(args.text, args.key)
+            else:
+                result = vigenere.decrypt(args.text)
+
+    elif args.procedure == "caesar":
+        caesar = Caesar()
+
+        if args.mode == "encrypt":
+            if args.key:
+                result = caesar.encrypt(args.text, args.key)
+            else:
+                result = caesar.encrypt(args.text)
+        elif args.mode == "decrypt":
+            if args.key:
+                result = caesar.decrypt(args.text, args.key)
+            else:
+                result = caesar.decrypt(args.text)
     print(result)
 
 
