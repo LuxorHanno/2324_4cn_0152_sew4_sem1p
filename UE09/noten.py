fff625e05760ea9416cd1e79d8f5a74ce094d836
hanno <0152@htl.rennweg.at>
2024-05-14 21:38:28 +0200

    UE: noten.py; Com.: 4 Fertigstellung des Python scrips inkl. PyDoc

--- UE09/noten.py
+++ UE09/noten.py
@@ -1,12 +1,12 @@
-__author__ = "Hanno Postl"
-__version__ = "1.0"
-__status__ = "Finished"
+"""
+This script is used to merge data from a CSV file and an XML file based on a common column.
+The CSV file contains grades, and the XML file contains student data.
+The script supports both verbose and quiet modes.
+The output is a CSV file that contains the merged data.
 
-import pandas as pd
-import argparse
-
-r"""noten = r'C:\Users\hanno\PycharmProjects\2324_4cn_0152_sew4_sem1p\UE09\res\noten.csv'
-schueler = r'C:\Users\hanno\PycharmProjects\2324_4cn_0152_sew4_sem1p\UE09\res\schueler.xml'
+Author: Hanno Postl
+Version: 1.4
+Status: Finished
 """
 
 import re
@@ -17,60 +17,67 @@ import sys
 
 
 def read_xml(filename: str) -> pd.DataFrame:
+    """
+    This function reads an XML file and extracts specific data using regular expressions.
+    The extracted data includes the student number, salutation, first name, last name, and date of birth.
+    The data is then returned as a pandas DataFrame.
+
+    :param filename: The name of the XML file to read.
+    :return: A DataFrame containing the extracted data.
+    """
     with open(filename, 'r', encoding='utf-8') as f:
         content = f.read()
 
-    pattern = re.compile("<Schueler>\s*<Nummer>(.*?)</Nummer>\s*<Anrede>(.*?)</Anrede>\s*<Vorname>(.*?)</Vorname>\s*<Nachname>(.*?)</Nachname>\s*<Geburtsdatum>(.*?)</Geburtsdatum>", flags=re.DOTALL)
+    pattern = re.compile(
+        "<Schueler>\s*<Nummer>(.*?)</Nummer>\s*<Anrede>(.*?)</Anrede>\s*<Vorname>(.*?)</Vorname>\s*<Nachname>(.*?)</Nachname>\s*<Geburtsdatum>(.*?)</Geburtsdatum>",
+        flags=re.DOTALL)
     result = re.findall(pattern, content)
-    df = pd.DataFrame(result, columns=['Nummer', 'Anrede', 'Vorname', 'Nachname', 'Geburtsdatum'], dtype=str)
-    return df
+    return pd.DataFrame(result, columns=['Nummer', 'Anrede', 'Vorname', 'Nachname', 'Geburtsdatum'], dtype=str)
+
 
 if __name__ == "__main__":
     # Argument parser for command line arguments
-    parser = argparse.ArgumentParser(description="noten.py by Hanno Postl / HTL Rennweg")
-    parser.add_argument("outfile", help="Ausgabedatei (z.B. result.csv)")
-    parser.add_argument("-n", help="csv-Datei mit den Noten")
-    parser.add_argument("-s", help="xml-Datei mit den Schülerdaten")
-    parser.add_argument("-m", default="Nummer", help="Name der Spalte, die zu verknüpfen ist (default = Nummer)")
-    parser.add_argument("-f", help="Name des zu filternden Gegenstandes (z.B. SEW)")
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(description="noten.py by Hanno Postl / HTL Rennweg")
+    parser.add_argument("outfile", help="Output file (e.g., result.csv)")
+    parser.add_argument("-n", help="CSV file with grades")
+    parser.add_argument("-s", help="XML file with student data")
+    parser.add_argument("-m", default="Nummer", help="Name of the column to join on (default = Nummer)")
+    parser.add_argument("-f", help="Name of the subject to filter (e.g., SEW)")
 
     # Group for output mode arguments
     outputgroup = parser.add_mutually_exclusive_group()
-    outputgroup.add_argument("-v", "--verbose", action="store_true", help="Gibt die Daten Kommandozeile aus")
-    outputgroup.add_argument("-q", "--quiet", action="store_true", help="keine Textausgabe")
+    outputgroup.add_argument("-v", "--verbose", action="store_true", help="Outputs the data to the command line")
+    outputgroup.add_argument("-q", "--quiet", action="store_true", help="No text output")
 
     # Parse the arguments
     args = parser.parse_args()
 
-    # Check if input file exists
+    # Check if input files exist
     if not os.path.exists(args.n):
         sys.stderr.write(args.n + ": " + os.strerror(2))
     elif not os.path.exists(args.s):
         sys.stderr.write(args.s + ": " + os.strerror(2))
     else:
-        # Read the input files
-        noten = pd.read_csv(args.n, sep=';', dtype=str, encoding='utf-8')
-        schueler = read_xml(args.s)
+        # Read the CSV and XML files into DataFrames
+        noten: pd.DataFrame = pd.read_csv(args.n, sep=';', dtype=str, encoding='utf-8')
+        schueler: pd.DataFrame = read_xml(args.s)
 
-
-        # Merge the dataframes
-        result = pd.merge(schueler, noten, on='Nummer')
-        print (result)
-
-        # Filter the data
+        # Merge the DataFrames based on the specified column
         if args.f:
-            result = result[result['Gegenstand'] == args.f]
+            result: pd.DataFrame = pd.merge(schueler, noten[["Nummer", args.f]], on='Nummer')
+        else:
+            result: pd.DataFrame = pd.merge(schueler, noten, on='Nummer')
 
-        # Write the result to a csv file
-        result.to_csv(args.outfile, sep=';',index=False)
+        # Write the result to the output file
+        result.to_csv(args.outfile, sep=';', index=False)
 
-        # If verbose mode is on
+        # Print the result if verbose mode is on
         if args.verbose:
-            print(f"""csv-Datei mit den Noten : {args.n}
-            xml-Datei mit den Schülerdaten : {args.s}
-            Name der Spalte, die zu verknüpfen ist : {args.m}
-            Output-Datei: {args.outfile}""")
+            print(f"""CSV file with grades : {args.n}
+            XML file with student data : {args.s}
+            Name of the column to join on : {args.m}
+            Output file: {args.outfile}""")
         elif args.quiet:
             pass
         else:
-            print(f"Output-Datei: {args.outfile}")
+            print(f"Output file: {args.outfile}")
